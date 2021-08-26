e98916ec82c77a6c3a3fc58ed8ce35f7980ecbc2
Shaheer kp <kpshaheer123@gmail.com>
2021-08-26 18:26:10 +0530

    final project

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,50 +267,37 @@ var number_val = false;
 var message_val = false;
 
 
-
-var u_name = document.getElementById("name")
-u_name.addEventListener('input', (e) => {
-  console.log(u_name.value);
-
-  var name_field = /^[a-zA-Z ]+$/;
+$("#name").on('input', function () {
+  this.value = this.value.replace(/[^a-zA-Z ]/, '')
 
 
-  if (u_name.value.charAt(0) == " ") {
-    name_err.innerHTML = "User name should not start with space"
-    document.getElementById("name").style.borderColor = "red"
+  if (this.value.charAt(0) == " ") {
+    $("#name_err").html("User name should not start with space")
+    $(this).css({ borderColor: "red" })
     name_val = false;
-
-  }
-  else if (u_name.value.length < 3) {
-    name_err.innerHTML = "User name should be atleast 2 character"
-    document.getElementById("name").style.borderColor = "red"
-    name_val = false;
-
   }
 
-  else if (u_name.value.length < 3) {
-    name_err.innerHTML = "User name should be atleast 2 character"
-    document.getElementById("name").style.borderColor = "red"
-    name_val = false;
-
-  }
-  else if (!u_name.value.match(name_field)) {
-    name_err.innerHTML = "invalid charaters"
-    document.getElementById("name").style.borderColor = "red"
-    name_val = false;
-
-  }
-  else if (u_name.value.length > 20) {
-    name_err.innerHTML = "User name should be max 20  character"
-    document.getElementById("name").style.borderColor = "red"
+  else if (this.value.length < 3) {
+    $("#name_err").text("User name should be atleast 2 character")
+    $(this).css({ borderColor: "red" })
     name_val = false;
 
   }
 
 
-  else if (u_name.value.includes("  ")) {
-    name_err.innerHTML = "User name should not have more spaces "
-    document.getElementById("name").style.borderColor = "red"
+
+
+  else if (this.value.length > 20) {
+    $("#name_err").text("User name should be max 20  character")
+    $(this).css({ borderColor: "red" })
+    name_val = false;
+
+  }
+
+
+  else if (this.value.includes("  ")) {
+    $("#name_err").text("User name should not have more spaces ")
+    $(this).css({ borderColor: "red" })
     name_val = false;
 
   }
@@ -318,8 +305,8 @@ u_name.addEventListener('input', (e) => {
 
 
   else {
-    document.getElementById("name").style.borderColor = "green"
-    name_err.innerHTML = " "
+    $(this).css({ borderColor: "green" })
+    $("#name_err").html("")
     name_val = true
   }
 })
@@ -331,75 +318,96 @@ u_name.addEventListener('input', (e) => {
 
 
 
-var u_email = document.getElementById("email")
-u_email.addEventListener('input', (e) => {
+
+
+
+
+
+
+
+
+
+
+
+
+$("#email").on('input', function () {
   var email_field = /^([\w-\.]+@([\w-]+\.)+[\w-]{1,20})?$/;
 
-  if (u_email.value.length < 0) {
-    email_err.innerHTML = "email should not be blank"
-    document.getElementById("email").style.borderColor = "red"
+  if (this.value.length < 0) {
+    email_err.html("email should not be blank")
+    $(this).css({ borderColor: "red" })
     email_val = false;
 
   }
+  else if (this.value.charAt(0) == " ") {
+    $("#email_err").html("Email should not start with space")
+    $(this).css({ borderColor: "red" })
+    name_val = false;
+  }
 
-  // else if (u_email.value.indexOf("@") == -1) {
-  //     email_err.innerHTML = "email should contain @"
-  //     email_val = false;
+  else if (this.value.includes("  ")) {
+    $("#email_err").text("Email should not have more 2 spaces ")
+    $(this).css({ borderColor: "red" })
+    name_val = false;
 
-  // }
+  }
 
-  else if (!email_field.test(u_email.value)) {
-    email_err.innerHTML = "invalid format"
-    document.getElementById("email").style.borderColor = "red"
+  else if (!email_field.test(this.value)) {
+    $("#email_err").html("invalid format")
+    $(this).css({ borderColor: "red" })
     email_val = false;
 
   }
   else {
-    email_err.innerHTML = " "
-    document.getElementById("email").style.borderColor = "green"
+    $("#email_err").html("")
+    $(this).css({ borderColor: "green" })
     email_val = true
   }
+
 })
 
 
-var u_number = document.getElementById("number")
-u_number.addEventListener('input', (e) => {
-  u_number.value = u_number.value.replace(/[^0-9]/, '').replace(/(\..*)\./, '$1');;
 
-  // if (number_field.test(u_number.value)) {
-  //     number_err.innerHTML = "invalid format"
-  //     number_val = false;
 
-  // }
-  if (u_number.value.length != 10) {
-    number_err.innerHTML = "Enter 10 numbers"
-    document.getElementById("number").style.borderColor = "red"
+$("#number").on('input', function () {
+  this.value = this.value.replace(/[^0-9]/, '').replace(/(\..*)\./, '$1');;
+  if (this.value.length != 10) {
+    $("#number_err").html("Enter 10 number")
+    $(this).css({ borderColor: "red" })
     number_val = false;
 
   }
+
   else {
-    number_err.innerHTML = " "
-    document.getElementById("number").style.borderColor = "green"
+    $("#number_err").html("")
+    $(this).css({ borderColor: "green" })
     number_val = true;
 
   }
+
+
 })
 
 
 
-var u_message = document.getElementById("message")
-u_message.addEventListener('input', (e) => {
-  if (u_message.value.length < 6) {
-    message_err.innerHTML = "message should be atleast 5 character"
-    document.getElementById("message").style.borderColor = "red"
 
+
+
+
+$("#message").on('input', function () {
+  if (this.value.length < 6) {
+    $("#message_err").html("Enter atleast 5 words")
+    $(this).css({ borderColor: "red" })
     message_val = false
   }
   else {
-    document.getElementById("message").style.borderColor = "green"
-    message_err.innerHTML = ""
+    $(this).css({ borderColor: "green" })
+    $("#message_err").html("")
     message_val = true;
   }
+
+
+
 
 })
 
@@ -412,9 +420,9 @@ u_message.addEventListener('input', (e) => {
 //form submission
 
 
-let form = document.getElementById("submit-form")
-form.addEventListener('submit', (e) => {
+$("#submit-form").submit((e) => {
   e.preventDefault()
+
   if (name_val == true && email_val == true && number_val == true && message_val == true) {
     $.ajax({
       url: "https://script.google.com/macros/s/AKfycbyv3tKlSQGHETWCB3WGcZxYpvWL8deSAvzYDnfXUpOs9CxfE1IaX5P0G0lJdEJP2GhUKg/exec",
@@ -435,11 +443,13 @@ form.addEventListener('submit', (e) => {
   else {
 
 
-    document.getElementById("name").style.borderColor = "red"
-    document.getElementById("email").style.borderColor = "red"
-    document.getElementById("number").style.borderColor = "red"
-    document.getElementById("message").style.borderColor = "red"
+    $("#name").css({ borderColor: "red" })
+    $("#email").css({ borderColor: "red" })
+    $("#number").css({ borderColor: "red" })
+    $("#message").css({ borderColor: "red" })
 
   }
+
+
 
 })
